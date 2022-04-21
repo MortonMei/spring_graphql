e8de0f5b85d7a75cba9ab616f9c9cc5e7b794b8a
MortonMei <1291247841@qq.com>
2022-04-21 21:11:31 +0800

    add resolver to data fetcher

--- src/main/java/com/example/spring_graphql/type/User.java
+++ src/main/java/com/example/spring_graphql/type/User.java
@@ -3,17 +3,23 @@ package com.example.spring_graphql.type;
 import com.example.spring_graphql.entity.UserEntity;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 public class User {
     private Integer id;
     private String email;
     private String password;
+    private List<Event> createdEvent = new ArrayList<>();
 
     public static User fromUserEntity(UserEntity userEntity) {
         User user = new User();
         user.setId(userEntity.getId());
         user.setEmail(userEntity.getEmail());
+
 //        user.setPassword(userEntity.getPassword());
+
         return user;
     }
 }
